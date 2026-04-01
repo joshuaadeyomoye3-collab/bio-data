d5c2b20ab9146309b9bc148512730ded4d512619
Joshua Oni-Adeyomoye <joshuaadeyomoye3@gmail.com>
2026-04-01 14:13:44 +0100

    bio-data entry 1

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Ndi Application System!")
+name = input("What is your name? ")
+dob = input("What is your date of birth? ")
+sex = input("what is your gender? ")
+school = input("What school did you attend? ")
+
+print("Thank you for using this application!")
