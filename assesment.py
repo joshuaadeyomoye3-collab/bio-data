17cdd59918f87fcbe2ca8e1faf8e8b7db383317d
Joshua Oni-Adeyomoye <joshuaadeyomoye3@gmail.com>
2026-04-02 09:42:14 +0100

    assesment

---
+++ assesment.py
@@ -0,0 +1,8 @@
+print('welcome to my conversion program')
+x= 3
+y=2.3
+
+a=float(x)
+b=int(y)
+print(a)
+print(b)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print("Welcome to Ndi Application System!")
 name = input("What is your name? ")
 dob = input("What is your date of birth? ")
-sex = input("what is your gender? ")
+sex = input("What is your gender? ")
+age = input("What is your age? ")
 school = input("What school did you attend? ")
 
-print("Thank you for using this application!")
+print(f"Thanks for applying, {name}! We've noted your details DOB: {dob}, sex: {sex}, school: {school}. Good luck,we'll be in touch!")
+
